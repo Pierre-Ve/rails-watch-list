b8b87f4f053c1a6e607e60a3cbde974a8c29f750
pierre veillet <pierre.veillet38@gmail.com>
2023-11-17 16:19:59 +0100

    css ameillioré

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,8 +5,10 @@ class BookmarksController < ApplicationController
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
     @bookmark.save
-    redirect_to lists_path
+    redirect_to list_path(@list)
   end
 
   def destroy
